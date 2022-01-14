2044d89084fc08ffc4ab02176ba140e743d866bd
Damian Connolly <16608228+Damo1155@users.noreply.github.com>
2022-01-14 15:30:44 +0000

    Initial implementation with base level pipeline work.

---
+++ Build/Webpack/webpack.config.js
@@ -0,0 +1,32 @@
+// TODO :   Utilise 'webpack-merge' to allow more customisable build pipelines
+
+const path = require("path");
+
+module.exports = {
+    mode: "production",
+    devtool: "source-map",
+    entry: {
+        "fetchlite": "./src/fetchlite.ts"
+    },
+    output: {
+        library: "FetchLite",
+        libraryTarget: "umd",
+        libraryExport: "default",
+        filename: "[name].js",
+        path: path.resolve(__dirname, "../../dist")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "babel-loader",
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".js"],
+        alias: {
+            
+        }
+    }
+}
